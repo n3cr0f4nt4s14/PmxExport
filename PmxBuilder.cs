63d6bf55a71640a97d492b0f731f547943421f0c
Necrofantasia <n3cr0f4nt4s14@gmail.com>
2021-12-17 14:09:31 +0100

    Removed namespace PmxExport and renamed files accordingly. The plugin now has a different GUID.

--- PmxBuilder.cs
+++ PmxBuilder.cs
@@ -1,7 +1,50 @@
-﻿using PmxLib;
+﻿using System.IO;
+using PmxLib;
 
-namespace PmxExport {
-    internal class PmxBuilder {
+/**
+* <summary>
+* The PmxBuilder.<br></br>
+* Responsible for building (constructing and saving) the PMX-file.
+* </summary>
+*/
+internal class PmxBuilder {
+	/** <summary>The default file path, where the PMX-file will be saved.</summary> */
+	const string DEFAULT_SAVE_PATH = "C:\\koikatsu_models\\";
+	Pmx pmxFile;
 
-    }
+	public PmxBuilder() {
+		this.pmxFile = new Pmx();
+	}
+
+	/**
+	* <summary>
+	* Builds the PMX-file.<br></br>
+	* TODO: specify result message.
+	* </summary>
+	* <returns>The result message.</returns>
+	*/
+	public string BuildStart() {
+		string msg = CreateDirectories("TODO");
+
+		return msg;
+	}
+
+	/**
+	* <summary>
+	* Creates all needed directories for the PMX-file.
+	* </summary>
+	*/
+	private string CreateDirectories(string path) {
+		string msg = "\n";
+
+		try {
+			Directory.CreateDirectory(path);
+			//TODO:
+		}
+		catch(IOException e) {
+			msg = e.ToString() + msg;
+		}
+
+		return msg;
+	}
 }
---
+++ PmxExport.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using BepInEx;
+
+[BepInPlugin(GUID, NAME, VERSION)]
+public class PmxExport : BaseUnityPlugin {
+	/** <summary>The pulugin's globally unique id.</summary> */
+	public const string GUID = "com.n3cr0f4nt4s14.pmxexport";
+	/** <summary>The plugin's name.</summary> */
+	public const string NAME = "PmxExport";
+	/** <summary>The pluginn's version.</summary> */
+	public const string VERSION = "0.0.1";
+
+	private PmxBuilder builder;
+	private PmxExportWindow window;
+
+	public void OnGUI() {
+		if(window == null) {
+			window = new PmxExportWindow();
+		}
+
+		window.DrawWindow();
+	}
+
+	public void Update() {
+		if(Input.GetKeyDown(PmxExportWindow.KEY)) {
+			window.Show();
+		}
+	}
+}
