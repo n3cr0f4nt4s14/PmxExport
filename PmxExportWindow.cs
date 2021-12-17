1810677039a697095a4befc2a6812ec75658e1b9
Necrofantasia <n3cr0f4nt4s14@gmail.com>
2021-12-17 15:12:38 +0100

    Added save path textfield.

--- PmxExportWindow.cs
+++ PmxExportWindow.cs
@@ -10,6 +10,7 @@ class PmxExportWindow {
 	private readonly Color GREY = new Color(0.5f, 0.5f, 0.5f, 1f);
 
 	private const string TITLE = PmxExport.NAME + " " + PmxExport.VERSION;
+	private const string TEXT_LABEL_SAVE_PATH = "Save path";
 	private const string TEXT_BUTTON_SAVE = "Save";
 	private const string TEXT_BUTTON_CANCLE = "Cancle";
 
@@ -20,18 +21,28 @@ class PmxExportWindow {
 
 	/** <summary>Onscreen rectangle denoting the window's position and size.</summary> */
 	private Rect window;
+	/** <summary>The rectangle denoting the save path label's position and size.</summary> */
+	private Rect labelSavePath;
+
+	/** <summary>The rectangle denoting the save path textfield's position and size.</summary> */
+	private Rect textFieldSavePath;
+
 	/** <summary>The rectangle denoting the save button's position and size.</summary> */
 	private Rect buttonSave;
 	/** <summary>The rectangle denoting the cancle button's position and size.</summary> */
 	private Rect buttonCancle;
+
 	/** <summary>Indicates whether or no the export window should be shown.</summary> */
 	private bool showExportWindow;
+	private string savePath;
 
 	public PmxExportWindow() {
 		const int sizeHalf = SIZE_WINDOW / 2;
 		window = new Rect(Screen.width / 2 - sizeHalf, Screen.height / 2 - sizeHalf, SIZE_WINDOW, SIZE_WINDOW);
 
 		//Folder, Browse, Name, Format, Save texture, Save position, Apply T-pose, Save, Close
+		labelSavePath = new Rect(MARGIN, 2 * MARGIN, SIZE_WINDOW * 0.2f - MARGIN, MARGIN);
+		textFieldSavePath = new Rect(labelSavePath.x + labelSavePath.width, labelSavePath.y, SIZE_WINDOW * 0.6f - MARGIN, MARGIN);
 
 		//Cancle and save button layout is calculated from the bottom of the window.
 		const int buttonWidth = SIZE_WINDOW - 2 * MARGIN;
@@ -61,6 +72,9 @@ class PmxExportWindow {
 		//https://docs.unity3d.com/ScriptReference/GUI.Window.html
 		//https://github.com/suiginsoft/COM3D2.ModelExportMMD/blob/master/COM3D2.ModelExportMMD.Gui/ModelExportWindow.cs
 		//https://github.com/suiginsoft/COM3D2.ModelExportMMD/blob/master/COM3D2.ModelExportMMD.Plugin/ModelExportPlugin.cs
+
+		GUI.Label(labelSavePath, TEXT_LABEL_SAVE_PATH);
+		savePath = GUI.TextField(textFieldSavePath, savePath);
 
 		if(GUI.Button(buttonSave, TEXT_BUTTON_SAVE)) {
 
